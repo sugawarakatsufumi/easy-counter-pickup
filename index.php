97e32dd8858e0995fc0e852dc8671787b78f1237
sugawara.katsufumi <sugawarakatsufumi@sugawarakatsufumi-no-MacBook-Air-3.local>
2023-03-16 09:35:38 +0900

    カスタムフィールド名を変数化した
    カスタムフィールド名を変数化した

--- index.php
+++ index.php
@@ -1,28 +1,32 @@
 <?php
 /*
 Plugin Name: ka2 Easy Counter & Pick up
-Description: 簡単なアクセスカウンター(tax&singularに対応)と記事にピックアップフラグを立てる、アンテナサイトなどに最適
+Description: 簡単なアクセスカウンター(tax&singularに対応)と記事にピックアップフラグを立てる、アンテナサイトなどに最適。カスタムフィールドにインサート、カウンターはデフォルト1ヶ月でリセットされます
 Author: 菅原勝文
 Version: 1.0
 */
-class easy_counter_pickup_plugin {
+class ka2EasyCounterPickupPlugin {
+  
+  public $accessCounterName = 'ka2AccessCounter';
+  public $accessCounterDate = 'ka2FirstAccessDate';
+  public $removesecond = 2592000;
+
   public function __construct(){
     add_filter( 'the_content', array($this,'debug'), 10, 1);
     add_filter( 'template_redirect', array($this,'accessCounter'), 10, 1);
     add_action('rest_api_init', array($this, 'api_add_fields'));
-   // add_action('rest_api_init', array($this, 'api_add_tax_fields'));
   }
   public function debug($str){
     global $post;
     if(is_singular()){
-      $key_1_value = get_post_meta( $post->ID, 'ka2AccessCounter', true );
-      $key_2_value = get_post_meta( $post->ID, 'ka2FirstAccessDate', true );
+      $key_1_value = get_post_meta( $post->ID, $this->accessCounterName, true );
+      $key_2_value = get_post_meta( $post->ID, $this->accessCounterDate, true );
       return "アクセス数".$key_1_value." 初回アクセス".$key_2_value.$str;
     }
   }
   public function api_add_fields(){
     register_rest_field(array('post','pages','category','tag'),
-      'ka2AccessCounter',
+      $this->accessCounterName,
       array(
         'get_callback' => function($post, $name){
           $counter = '';
@@ -44,16 +48,16 @@ class easy_counter_pickup_plugin {
   }
   public function accessCounter(){
     global $post;
-    $removesecond = 2592000;
+    $removesecond = $this->removesecond;
     if(is_singular()){
-      $accessCount = get_post_meta( $post->ID, 'ka2AccessCounter', true );
-      $firstAccess = get_post_meta( $post->ID, 'ka2FirstAccessDate', true );
+      $accessCount = get_post_meta( $post->ID, $this->accessCounterName, true );
+      $firstAccess = get_post_meta( $post->ID, $this->accessCounterDate, true );
       if(!$firstAccess){
-        update_post_meta($post->ID, 'ka2FirstAccessDate', date('Y-m-d H:i:s'));
+        update_post_meta($post->ID, $this->accessCounterDate, date('Y-m-d H:i:s'));
       }
       if( strtotime($firstAccess)+$removesecond < strtotime(date('Y-m-d H:i:s')) ){
         $accessCount = 1;
-        update_post_meta($post->ID, 'ka2FirstAccessDate', date('Y-m-d H:i:s'));
+        update_post_meta($post->ID, $this->accessCounterDate, date('Y-m-d H:i:s'));
       }else{
         if($accessCount){
           $accessCount = $accessCount+1;
@@ -61,17 +65,17 @@ class easy_counter_pickup_plugin {
           $accessCount = 1;
         }
       }
-      update_post_meta( $post->ID, 'ka2AccessCounter', $accessCount);
+      update_post_meta( $post->ID, $this->accessCounterName, $accessCount);
     }elseif(is_tax() || is_category() || is_tag()){
       $term_id = get_queried_object_id();
-      $accessCount = get_term_meta( $term_id, 'ka2AccessCounter', true );
-      $firstAccess = get_term_meta( $term_id, 'ka2FirstAccessDate', true );
+      $accessCount = get_term_meta( $term_id, $this->accessCounterName, true );
+      $firstAccess = get_term_meta( $term_id, $this->accessCounterDate, true );
       if(!$firstAccess){
-        update_term_meta($term_id, 'ka2FirstAccessDate', date('Y-m-d H:i:s'));
+        update_term_meta($term_id, $this->accessCounterDate, date('Y-m-d H:i:s'));
       }
       if( strtotime($firstAccess)+$removesecond < strtotime(date('Y-m-d H:i:s')) ){
         $accessCount = 1;
-        update_term_meta($term_id, 'ka2FirstAccessDate', date('Y-m-d H:i:s'));
+        update_term_meta($term_id, $this->accessCounterDate, date('Y-m-d H:i:s'));
       }else{
         if($accessCount){
           $accessCount = $accessCount+1;
@@ -79,9 +83,8 @@ class easy_counter_pickup_plugin {
           $accessCount = 1;
         }
       }
-      update_term_meta( $term_id, 'ka2AccessCounter', $accessCount);
+      update_term_meta( $term_id, $this->accessCounterName, $accessCount);
     }
   }
 }
-
-$easy_counter_pickup_plugin = new easy_counter_pickup_plugin();
+$ka2EasyCounterPickupPlugin = new ka2EasyCounterPickupPlugin();
